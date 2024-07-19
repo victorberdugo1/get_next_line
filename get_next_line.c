ba6e56fdcb9e9c0786c927d80e279b0078da9fd8
Victor <victor@localhost.localdomain>
2024-07-20 00:20:46 +0200

    com

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,14 @@
 /*   By: vberdugo <vberdugo@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 10:08:46 by vberdugo          #+#    #+#             */
-/*   Updated: 2024/07/13 17:58:29 by vberdugo         ###   ########.fr       */
+/*   Updated: 2024/07/19 10:28:29 by victor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <unistd.h>
+#include <fcntl.h>
+
 
 char *get_next_line(int fd)
 {
