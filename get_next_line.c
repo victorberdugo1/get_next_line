b1c49701fcee56a195be0b0e0032d61ca77b2b42
Victor Berdugo <vberdugo@car6s6.42barcelona.com>
2024-07-22 13:18:32 +0200

    cambio

--- get_next_line.c
+++ get_next_line.c
@@ -6,19 +6,30 @@
 /*   By: vberdugo <vberdugo@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 10:08:46 by vberdugo          #+#    #+#             */
-/*   Updated: 2024/07/19 10:28:29 by victor           ###   ########.fr       */
+/*   Updated: 2024/07/22 12:20:01 by vberdugo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <unistd.h>
-#include <fcntl.h>
+#include <stdlib.h> 
 
+char	*da_line(char *buffrd)
+{
+	return (buffrd);
+}
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
 	static char	*line;
+	ssize_t		bytes_read;
 
-	line = (void*)0;
+	line = (char *)malloc(BUFFER_SIZE + 1);
+	if (line == NULL)
+    	return NULL;
+	bytes_read = read(fd, line,BUFFER_SIZE);
+	if (bytes_read < 0)
+		free(line);
+	da_line(line);
 	return (line);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: vberdugo <vberdugo@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 17:28:01 by vberdugo          #+#    #+#             */
-/*   Updated: 2024/07/19 10:28:03 by victor           ###   ########.fr       */
+/*   Updated: 2024/07/22 12:44:29 by vberdugo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,21 @@
 #include <stdlib.h>
 #include "get_next_line.h"
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     int fd;
     char *line;
 
-    if (argc != 2)
+    if (argc == 1)
+    	fd = 0; 
+	else
     {
-        printf("Uso: %s nombre_archivo\n", argv[0]);
-        return 1;
+        fd = open(argv[1], O_RDONLY);
+        if (fd < 0)
+            return 1;
     }
-    fd = open(argv[1], O_RDONLY);
-    if (fd < 0)
-        return (1);
-    while ((line = get_next_line(fd)) != NULL)
-    {
-        printf("%s\n", line);
-        free(line);
-    }
+    line = get_next_line(fd);
+	printf("%s", line);
+    free(line);
     close(fd);
-    return (0);
+    return 0;
 }
