02c5f19dcbe521fd76d0f96c9809b13e0fae8041
Victor <victor@localhost.localdomain>
2024-07-24 21:22:02 +0200

    add

--- get_next_line.c
+++ get_next_line.c
@@ -6,57 +6,127 @@
 /*   By: vberdugo <vberdugo@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 10:08:46 by vberdugo          #+#    #+#             */
-/*   Updated: 2024/07/24 16:02:26 by vberdugo         ###   ########.fr       */
+/*   Updated: 2024/07/24 21:11:04 by victor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <unistd.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 
-int da_line(char **line) {
-	char *newline_pos;
-	char *remainder;
-	newline_pos = ft_strchr(*line, '\n');
-	if (newline_pos != NULL) {
-		newline_pos++;
-		remainder = ft_strdup(newline_pos);
-		if (remainder == NULL)
-			return -1;
-		*(newline_pos - 1) = '\0';
-		free(*line);
-		*line = remainder;
-		return 0;
+static char	*get_line(char *buffer)
+{
+	int		i;
+	char	*line;
+
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	if (buffer[i] == '\n')
+		i++;
+	line = (char *)malloc(i + 1);
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+	{
+		line[i] = buffer[i];
+		i++;
 	}
-	return 1;
+	if (buffer[i] == '\n')
+	{
+		line[i] = '\n';
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
 }
 
-char *get_next_line(int fd) {
-	static char *line;
-	char *temp;
-	ssize_t bytes_read;
+static char	*update_buffer(char *buffer)
+{
+	int		i;
+	int		j;
+	char	*new_buffer;
 
-	line = (char *)malloc(BUFFER_SIZE + 1);
-	if (line == NULL)
-		return NULL;
-	while ((bytes_read = read(fd,line , BUFFER_SIZE)) > 0) {
-		//line[bytes_read] = '\0';
-		temp = ft_strjoin(line, temp);
-		free(line);
-		line = temp;
-		if (ft_strchr(line, '\n') != NULL)
-			break;
-	}
-	if (bytes_read < 0 || (bytes_read == 0 && line[0] == '\0'))
+	i = 0;
+	j = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	if (buffer[i] == '\n')
+		i++;
+	if (buffer[i] == '\0')
 	{
-		free(line);
-		line = NULL;
-		return NULL;
+		free(buffer);
+		return (NULL);
 	}
-	line = ft_strdup(temp);
-	if (da_line(&line) == -1) {
-		free(line);
-		return NULL;
+	new_buffer = (char *)malloc(ft_strlen(buffer) - i + 1);
+	if (!new_buffer)
+		return (NULL);
+	while (buffer[i])
+		new_buffer[j++] = buffer[i++];
+	new_buffer[j] = '\0';
+	free(buffer);
+	return (new_buffer);
+}
+
+static char	*readln2(int fd, char *temp_buffer, char **buffer)
+{
+	char	*tmp;
+	int		bytes_read;
+
+	while (!ft_strchr(*buffer, '\n'))
+	{
+		bytes_read = read(fd, temp_buffer, BUFFER_SIZE);
+		if (bytes_read <= 0)
+		{
+			if (bytes_read < 0)
+				return (NULL);
+			break ;
+		}
+		temp_buffer[bytes_read] = '\0';
+		tmp = ft_strjoin(*buffer, temp_buffer);
+		free(*buffer);
+		if (!tmp)
+			return (NULL);
+		*buffer = tmp;
 	}
+	return (*buffer);
+}
+
+char	*readnl(int fd, char **buffer)
+{
+	char	*temp_buffer;
+
+	temp_buffer = (char *)malloc(BUFFER_SIZE + 1);
+	if (!temp_buffer)
+		return (NULL);
+	if (!readln2(fd, temp_buffer, buffer))
+	{
+		free(temp_buffer);
+		return (NULL);
+	}
+	free(temp_buffer);
+	return (*buffer);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer;
+	char		*line;
+
+	if (!buffer)
+		buffer = ft_strdup("");
+	if (readnl(fd, &buffer) == NULL)
+		return (NULL);
+	if (buffer[0] == '\0')
+	{
+		free(buffer);
+		buffer = NULL;
+		return (NULL);
+	}
+	line = get_line(buffer);
+	if (!line)
+		return (NULL);
+	buffer = update_buffer(buffer);
 	return (line);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: vberdugo <vberdugo@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 17:28:01 by vberdugo          #+#    #+#             */
-/*   Updated: 2024/07/24 15:52:18 by vberdugo         ###   ########.fr       */
+/*   Updated: 2024/07/24 21:20:03 by victor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,29 @@
 #include <stdlib.h>
 #include "get_next_line.h"
 
-int main(int argc, char **argv) {
-    int fd;
-    char *line;
-    if (argc == 1)
-        fd = 0;
-    else {
-        fd = open(argv[1], O_RDONLY);
-        if (fd < 0)
-            return 1;
-    }
-    while ((line = get_next_line(fd)) != NULL) {
-        printf("%s\n", line);
-        free(line);
-    }
-    close(fd);
-    return 0;
+int	main(int argc, char **argv)
+{
+	int		fd;
+	char	*line;
+
+	if (argc == 1)
+		fd = 0;
+	else
+	{
+		fd = open(argv[1], O_RDONLY);
+		if (fd < 0)
+			return (1);
+	}
+	line = get_next_line(fd);
+	while (line != NULL)
+	{
+		printf("%s", line);
+		free(line);
+		line = get_next_line(fd);
+	}
+	if (fd != 0)
+	{
+		close(fd);
+	}
+	return (0);
 }
