bb38104f6d0e5461cc9732929994431058d3af80
Victor Berdugo <vberdugo@car1s2.42barcelona.com>
2024-07-24 16:40:11 +0200

    m�f��p�֠���1^x86_64

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: vberdugo <vberdugo@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 10:08:46 by vberdugo          #+#    #+#             */
-/*   Updated: 2024/07/23 14:23:55 by vberdugo         ###   ########.fr       */
+/*   Updated: 2024/07/24 16:02:26 by vberdugo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,51 +14,49 @@
 #include <unistd.h>
 #include <stdlib.h> 
 
-int da_line(char **line)
-{
-    char *newline_pos;
-    char *remainder;
-
-    newline_pos = ft_strchr(*line, '\n');
-    if (newline_pos != NULL)
-    {
-        *newline_pos = '\0';
-        remainder = ft_strdup(newline_pos + 1);
-        if (remainder == NULL)
-            return -1;
-        free(*line);
-        *line = remainder;
-        return 0;
-    }
-    return 1;
+int da_line(char **line) {
+	char *newline_pos;
+	char *remainder;
+	newline_pos = ft_strchr(*line, '\n');
+	if (newline_pos != NULL) {
+		newline_pos++;
+		remainder = ft_strdup(newline_pos);
+		if (remainder == NULL)
+			return -1;
+		*(newline_pos - 1) = '\0';
+		free(*line);
+		*line = remainder;
+		return 0;
+	}
+	return 1;
 }
 
-char	*get_next_line(int fd)
-{
-	static char	*line;
-	char *buffer;
+char *get_next_line(int fd) {
+	static char *line;
 	char *temp;
-	ssize_t		bytes_read;
+	ssize_t bytes_read;
 
 	line = (char *)malloc(BUFFER_SIZE + 1);
 	if (line == NULL)
-		return (NULL);
-	while ((bytes_read = read(fd, line, BUFFER_SIZE)) > 0)
-	{	
-		temp = ft_strjoin(buffer, line);
-        free(line);
-        line = temp;
-        if (da_line(&line) == 0)
-            break;
+		return NULL;
+	while ((bytes_read = read(fd,line , BUFFER_SIZE)) > 0) {
+		//line[bytes_read] = '\0';
+		temp = ft_strjoin(line, temp);
+		free(line);
+		line = temp;
+		if (ft_strchr(line, '\n') != NULL)
+			break;
 	}
-	if (bytes_read < BUFFER_SIZE)
+	if (bytes_read < 0 || (bytes_read == 0 && line[0] == '\0'))
 	{
-		if (bytes_read < 0)
-		{
-			free(line);
-			return (NULL);
-		}
-
+		free(line);
+		line = NULL;
+		return NULL;
+	}
+	line = ft_strdup(temp);
+	if (da_line(&line) == -1) {
+		free(line);
+		return NULL;
 	}
 	return (line);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: vberdugo <vberdugo@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 17:28:01 by vberdugo          #+#    #+#             */
-/*   Updated: 2024/07/22 15:02:37 by vberdugo         ###   ########.fr       */
+/*   Updated: 2024/07/24 15:52:18 by vberdugo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,20 @@
 #include <stdlib.h>
 #include "get_next_line.h"
 
-int	main(int argc, char **argv)
-{
-	int		fd;
-	char	*line;
-
-	if (argc == 1)
-		fd = 0;
-	else
-	{
-		fd = open(argv[1], O_RDONLY);
-		if (fd < 0)
-			return (1);
-	}
-	line = get_next_line(fd);
-	printf("%s", line);
-	free(line);
-	close(fd);
-	return (0);
+int main(int argc, char **argv) {
+    int fd;
+    char *line;
+    if (argc == 1)
+        fd = 0;
+    else {
+        fd = open(argv[1], O_RDONLY);
+        if (fd < 0)
+            return 1;
+    }
+    while ((line = get_next_line(fd)) != NULL) {
+        printf("%s\n", line);
+        free(line);
+    }
+    close(fd);
+    return 0;
 }
